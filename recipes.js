69bf3d2745e420d546245c2247f9c711e525f964
outtaspacecode <cole.beebe@protonmail.com>
2025-12-26 21:23:22 -0700

    Initial commit; I just don't want to lose progress on recipes

---
+++ recipes.js
@@ -0,0 +1,409 @@
+/*
+    Equivalent Measurments
+    
+    Measure                   | Equvalent
+    --------------------+----------------------------------
+    Under 1/8 teaspoon  | Dash
+    3 teaspoons         | 1 tablespoon
+    4 tablespoons       | 1/4 cup = 2 ounces
+    5 1/3 tablespoons   | 1/3 cup
+    8 tablespoons       | 1/2 cup = 4 ounces
+    10 2/3 tablespoons  | 2/3 cup
+    12 tablespoons      | 3/4 cup = 6 ounces
+    16 tablespoons      | 1 cup = 8 ounces
+    --------------------+----------------------------------
+    1/8 cup             | 1 ounce = 2 tablespoons
+    1/4 cup             | 2 ounces = 4 tablespoons
+    1/3 cup             | 5 tablespoons plus 1 teaspoon
+    1/2 cup             | 4 ounces = 8 tablespoons
+    3/4 cup             | 6 ounces = 12 tablespoons
+    1 cup               | 8 ounces = 16 tablespoons
+    2 cups              | 16 ounces = 1 pint
+    4 cups              | 32 ounces = 1 quart
+    8 cups              | 64 ounces = 2 quarts = 1/2 gallon
+    16 cups             | 128 ounces = 4 quarts = 1 gallon
+*/
+export const recipes = [
+    {
+        title: "Bacon Grean Pea Pasta Salad",
+        ingredients: [
+            "8 slices thick-cut bacon, cut into 1/4\" pieces",
+            "12 oz. dry small pasta shells",
+            "12 oz. frozen petite green peas (thawed)",
+            "1/3 c. red onion, small diced",
+            "1 c. mayonnaise",
+            "1/4 c. olive oil",
+            "2 T. fresh-squeezed lemon juice",
+            "1 T. Dijon mustard",
+            "3/4 t. salt",
+            "1/2 t. ground black pepper",
+            "2 T. fresh parsely, finely chopped",
+            "3 T. fresh chives, finely chopped"
+        ],
+        directions: [
+            "Cook cut bacon in a large skillet over medium-high heat until crispy, and place on a paper towel-lined plate to drain. Set aside.",
+            "Cook pasta shells in boiling, salted water as directed on the pasta packaging. Pour pasta into a large colander and rinse under cold water until chilled and set aside.",
+            "Make the dressing by whisking together the mayonnaise, olive oil, lemon juice, mustard, salt and pepper until smooth and then stir in the parsley and chives.",
+            "Prepare the salad by placing the pasta shells in a large mixing bowl and adding the bacon, peas and red onion. Pour the dressing into the bowl and toss to combine until all the ingredients are well distributed and coated in the dressing."
+        ]
+    },
+    {
+        title: "Cheesy Potatoes",
+        ingredients: [
+            "1 bag frozen hash browns",
+            "3 cans cream of chicken soup", 
+            "2 c. cheddar cheese",
+            "4 T. melted butter",
+            "Pinch of salt and pepper"
+        ],
+        directions: [
+            "Heat oven to 350°. Grease 9x13 pan.",
+            "Mix hash browns, cream of chicken soup, cheddar cheese, butter and salt and pepper together.",
+            "Add to 9x13 inch pan and bake for 45 minutes or until golden brown."
+        ]
+    },
+    {
+        title: "Potato Casserole",
+        ingredients: [
+            "8-10 potatoes, boiled in jackets (or frozen hashbrowns)",
+            "8 oz. container sour cream",
+            "1 can cream of chicken soup",
+            "1/2 stick butter",
+            "4 green onions, chopped (1/4 c.)",
+            "1-2 c. grated cheese",
+            "Crumbled corn flakes"
+        ],
+        directions: [
+            "Preheat oven to 350°. Slice cooked potatoes into 9x13 pan.", 
+            "Heat sour cream, soup, butter, and onions in a sauce pan; pour over potatoes.",
+            "Cover with grated cheese.",
+            "Bake 30 min.",
+            "(Optional) Add 1-2 c. leftover ham (or a can of spam) to potato layers."
+        ]
+    },
+    {
+        title: "Sweet-n-Sour Sauce",
+        alt: "Mom's Special Recipe",
+        ingredients: [
+            "2 t. soy sauce",
+            "6 T. vinegar",
+            "2 T. cornstarch",
+            "4 T. ketchup",
+            "8 T. sugar",
+            "1 c. pineapple juice",
+            "1 can pineapple chunks",
+            "1 11 oz. can mandarin oranges"
+        ],
+        directions: [
+            "Before putting on heat, mix all ingredients except fruit (if there is not enough pinapple juice in the can, top off with juice from mandarin oranges).",
+            "Stir well (until all cornstarch is fully mixed in).",
+            "Put on heat. Stir until thick.",
+            "Serve over rice"
+        ]
+    },
+    {
+        title: "Steak on a Stick",
+        ingredients: [
+            "3/4 c. soy sauce",
+            "1/2 T. vinegar",
+            "1/4 c. sugar",
+            "1 t. ginger",
+            "1/2 T. olive oil",
+            "Garlic salt",
+            "2 lb. wafer-thin beef slices"
+        ]
+    },
+    {
+        title: "Chicken Crescents",
+        ingredients: [
+            "8 oz. cream cheese, softened",
+            "2 T. butter, softened",
+            "2 1/2 c. cooked, cubed chicken",
+            "1/4 t. salt",
+            "1/8 t. pepper",
+            "2 green onions, sliced",
+            "4 oz. sliced pimientos, drained",
+            "2 cans (8 oz. each) refrigerator crescent rolls",
+            "2 T. melted butter",
+            "1 1/2 c. seasoned croutons, crushed"
+        ],
+        directions: [
+            "Preheat oven to 350°",
+            "Combine cream cheese, butter, chicken, salt, pepper, onions, and pimientos.",
+            "Roll crescent rolls out. Put heaping tablespoon of mixture on each. Roll into crescent form and place on baking sheet.",
+            "Top each crescent with butter and crumbs.",
+            "Bake 20-25 min.",
+            "(Optional) Top with <a href='#'>mushroom gravy</a>."
+        ]
+    },
+    {
+        title: "Mushroom Gravy",
+        ingredients: [
+            "8 oz. butter, melted",
+            "16 oz. sliced mushrooms (8 oz. will work)",
+            "4 T. flour",
+            "2 c. double strength chicken bouillon granules"
+        ],
+        directions: [
+            "Sauté mushrooms in butter; stir in flour",
+            "Add liquid and stir until thickened"
+        ]
+    },
+    {
+        title: "Chicken and Rice",
+        ingredients: [
+            "2-3 c. Minute rice",
+            "2-3 cans of mushroom soup",
+            "1 packet Lipton onion soup"
+        ],
+        directions: [
+            "Preheat oven to 350°",
+            "Mix rice and soup and 1/2 onion soup in casserole dish.",
+            "Put 2-4 chicken breasts on top. Sprinkle rest of onion soup on top.",
+            "Bake for 1 1/2-2 hours (depending on amount of chicken)."
+        ]
+    },
+    {
+        title: "Chicken Chalupas",
+        author: "Gayle Scott",
+        ingredients: [
+            "Flour tortillas",
+            "3/4 c. jack cheese, grated",
+            "4 large chicken breasts, cooked and broken",
+            "2 cans cream of chicken soup",
+            "2 c. sour cream",
+            "1/4 c. green onions, chopped",
+            "1 can sliced or chopped black olives",
+            "1 small can chopped green chilies",
+            "1 c. grated cheese"
+        ],
+        directions: [
+            "Preheat oven to 350°",
+            "Mix soup, sour cream, cheeses, onions, olives, and chilies. Set aside 2 cups.",
+            "Add chicken to remaining mixture.",
+            "Fill and roll tortillas; place in 9x13 pan.",
+            "Pour reserved sauce over tortillas. Cover with cheese.",
+            "Cover with foil and bake for 40 minutes."
+        ]
+    },
+    {
+        title: "Stuffed Manicotti",
+        author: "Tracy Meng",
+        ingredients: [
+            "8 uncooked manicotti",
+            "1/2 lb mozzarella cheese",
+            "1/2 c. cottage cheese",
+            "1/3 c. parmesan",
+            "1/2 t. salt",
+            "Pepper (optional)",
+            "1 (8 oz.) can tomato sauce",
+            "1 package of spaghetti mix",
+            "2 beaten eggs",
+            "2 T. margarine"
+        ],
+        directions: [
+            "Preheat oven to 350°",
+            "Combine tomato sauce and spaghetti mix and heat until boiling.",
+            "Combine all but noodles and sauce. Stuff uncooked manicotti and put in pan.",
+            "Pour sauce over noodles. Sprinkle with parmesan.",
+            "Cover with foil and bake for 45-50 minutes."
+        ]
+    },
+    {
+        title: "Barbecue Roast Beef Sandwiches",
+        ingredients: [
+            "Round or rump roast",
+            "Lawry's seasoning salt",
+            "K.C. Masterpiece barbecue sauce (or Bull's Eye barbecue sauce)"
+        ],
+        directions: [
+            "Preheat oven to 275°-300°",
+            "Brown with no salt or pepper. Douse with Lawry's on top side. Put in baking pan.",
+            "Bake for 6-10 hours. Do not add any water or liquid.",
+            "When finished, shred and add barbecue sauce.",
+            "Serve on hamburger buns."
+        ]
+    },
+    {
+        title: "Cheese and Broccoli Soup",
+        author: "Robyn Meng",
+        ingredients: [
+            "2 c. chicken broth or water with bouillon (3 cubes)",
+            "5 c. water",
+            "2 1/2 c. potatoes diced and peeled",
+            "1/2 t. sugar",
+            "2 1/2 c. broccoli (fresh or frozen)",
+            "1/2 c. water",
+            "1/2 c. flour",
+            "8 oz. velveeta, cut up"
+        ],
+        directions: [
+            "Combine and cook chicken broth, water, potatoes, sugar, and broccoli.",
+            "Mix flour and water and add to soup slowly",
+            "Add velveeta (more than 8 oz. can be used if desired) and allow cheese to melt."
+        ]
+    },
+    {
+        title: "Potato Soup",
+        ingredients: [
+            "12 chicken bouillon cubes",
+            "3 quarts water",
+            "1 T. parsely flakes",
+            "1 T. minced onions",
+            "1 large carrot grated",
+            "10-12 potatoes, peeled and cubed (dice size)",
+            "1 can canned milk",
+            "Milk",
+            "2 cubes butter",
+            "1 c. flour"
+        ],
+        directions: [
+            "Place all ingredients except milk, canned milk, butter, and flour in large 8 quart pot. Cook until potatoes are tender (about 20 min.).",
+            "Add canned milk combined with enough milk to measure a quart.",
+            "In a saucepan, melt butter. Add flour. Stir into soup until well blended.",
+            "Top soup with grated cheese when serving."
+        ]
+    },
+    {
+        title: "Chicken Noodle Soup",
+        alt: "(Whole recipe)",
+        ingredients: [
+            "3 chicken breasts",
+            "1/2 t. salt",
+            "3 q. water",
+            "8 chicken bouillon cubes",
+            "Celery",
+            "Carrots",
+            "Potatoes",
+            "1 T. parsley flakes",
+            "1 T. dried onion",
+            "4 handfuls country noodles",
+            "Frozen peas",
+            "Garlic salt (to taste)",
+            "1 cube butter",
+            "1/2 c. flour"
+        ],
+        directions: [
+            "Cook chicken breasts with salt and water.",
+            "Remove chicken from water and shred. Add chicken bouillon cubes to water.",
+            "Add celery, carrots, potatoes, parsely flakes, and dried onion to broth. Cook until vegetables are tender.",
+            "Cook noodles seperate according to directions.",
+            "Add frozen peas, cooked noodles, and shredded chicken to broth.",
+            "Add garlic salt to taste",
+            "Combine butter and flour. Stir into soup until well blended."
+        ]
+    },
+    {
+        title: "Beef Taco Skillet",
+        ingredients: [
+            "1 lb. ground beef",
+            "Taco seasoning",
+            "1 can (10 3/4 oz.) tomato soup",
+            "1/2 c. salsa",
+            "1/2 c. water",
+            "6 flour tortillas cut into 1\" pieces",
+            "1/2 c. shredded cheddar cheese",
+            "(Optional) 1/2 c. sour cream"
+        ],
+        directions: [
+            "Cook and drain beef. Season as instructed with taco seasoning.",
+            "Stir in soup, salsa, water, and tortillas. Heat to a boil.",
+            "Reduce heat to low and cook for 5 min. Stir.",
+            "Top with cheese.",
+            "(Optional) Stir in sour cream."
+        ]
+    },
+    {
+        title: "Cheeseburger Joes",
+        author: "food & family - Fall 2004, p.26",
+        ingredients: [
+            "1 lb. lean ground beef",
+            "1/2 c. barbecue sauce",
+            "1/4 c. ketchup",
+            "Cheese",
+            "Hamburger buns"
+        ],
+        directions: [
+            "Brown meat; drain.",
+            "Add barbecue sauce and ketchup. Cook until heated, stirring occasionally. Add more sauce if needed.",
+            "Serve on hamburger bun topped with cheese."
+        ]
+    },
+    {
+        title: "Ham and Cheddar in a Loaf",
+        author: "food & family - Fall 2004, p.18",
+        ingredients: [
+            "1 lb. frozen pizza dough, thawed",
+            "1 package (9 oz.) deli ham",
+            "1 c. shredded cheese",
+            "1/4 c. Miracle Whip",
+            "1 egg, beaten",
+            "1 T. parmesan cheese"
+        ],
+        directions: [
+            "Preheat oven to 350°",
+            "Place dough on lightly floured surface; flatten slightly with hands.",
+            "roll into 12x8 rectangle with rolling pin.",
+            "Top dough evenly with ham, overlapping slices slightly an dleaving a 1/2\" border around all sides.",
+            "Mix cheese and Miracle Whip; spread over ham",
+            "Moisten edges of dough with water. Starting at one of the long sides of dough, fold 1/3 of the dough over the filling, then repeat with the other side of dough.",
+            "Firmly pinch ends of dough together to seal. Place seam-side-down on lightly greased baking sheet.",
+            "Cut 3 diagonal slits on top; Brush with egg; Sprinkle with parmesan cheese.",
+            "Bake 35-40 min. Cool 10 min. before cutting."
+        ]
+    },
+    {
+        title: "Cheesy Meatball Skillet",
+        author: "food & family - Holiday 2006, p.71",
+        ingredients: [
+            "1 lb. lean ground beef",
+            "10 Saltine crackers, crushed",
+            "1/4 c. Kraft Zesty Italian dressing",
+            "1 beef bouillon cube, dessolved in 1/2 c. boiling water",
+            "1/2 c. sour cream",
+            "1 c. shredded cheese"
+        ],
+        directions: [
+            "Mix meat, crackers, and half of the dressing until well blended. Shape into thirty-two 1\" balls.",
+            "Cook meatballs in nonstick skillet on medium-high heat 8 min. or until browned, turning occasionally.",
+            "Add remaining dressing and bouillon; simmer 5 min. or until meatballs are cooked through.",
+            "Stir in sour cream; cook 30 sec. or until heated through, but not simmering, stirring frequently.",
+            "Remove from heat. Sprinkle with cheese. Serve over curly noodles."
+        ]
+    },
+    {
+        title: "Enchiladas",
+        ingredients: [
+            "1 can black beans",
+            "1 can refried beans",
+            "1 can tomato sauce",
+            "1 can green chilies",
+            "1/4 c. taco seasoning",
+            "Tortillas",
+            "1 can green enchilada sauce"
+        ],
+        directions: [
+            "Preheat oven to 350°",
+            "Mix together all ingredients (except tortillas and enchilada sauce) and warm on stove.",
+            "Fill tortillas. Cover bottom of a 9x13 pan well with some of the enchilada sauce.",
+            "Add filled tortillas to pan; Cover with remaining enchilada sauce.",
+            "Cover with foil and bake for 30 min."
+        ]
+    },
+    {
+        title: "Ham Fried Rice",
+        ingredients: [
+            "2 eggs, lightly beaten",
+            "1 1/2 t. vegetable oil",
+            "3/4 c. cold cooked rice",
+            "2/3 c. diced fully cooked ham",
+            "3/4 t. garlic powder",
+            "1/4 t. ground ginger",
+            "Dash pepper",
+            "1 1/2 t. soy sauce"
+        ],
+        directions: [
+
+        ]
+    }
+];
---
+++ script.js
@@ -0,0 +1,32 @@
+import { recipes } from './recipes.js';
+
+const main = document.querySelector("main");
+let content = "";
+
+recipes.sort((a, b) => a.title.localeCompare(b.title));
+
+recipes.forEach(recipe => {
+    content += `
+        <div class="recipe" >
+            <h2>${recipe.title}</h2>
+            ${recipe.alt ? `<h3>${recipe.alt}</h3>` : ``}
+            ${recipe.author ? `<h3>${recipe.author}</h3>` : ``}
+            <h3>Ingredients</h3>
+            <ul>
+                ${recipe.ingredients.map(ingredient => {
+                    return `<li>${ingredient}</li>`;
+                }).join('')}
+            </ul>
+            ${recipe.directions? `
+                <h3>Directions</h3>
+                <ol>
+                    ${recipe.directions.map(direction => {
+                        return `<li>${direction}</li>`;
+                    }).join('')}
+                </ol>
+            ` : ``}
+        </div>
+    `;
+});
+
+main.innerHTML += content;
