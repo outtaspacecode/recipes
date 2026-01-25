43c2a4dcab2cd1a06d296f8540423b63da21df3e
outtaspacecode <cole.beebe@protonmail.com>
2026-01-25 14:55:30 -0500

    I hope this fixed the path issues

--- js/recipe.js
+++ js/recipe.js
@@ -5,7 +5,7 @@ async function init() {
     // Search for parameter 'name' in the url
     const params = new URLSearchParams(window.location.search);
     const recipeName = params.get('name');
-    const recipes = await getJson('../recipes.json');
+    const recipes = await getJson('./recipes.json');
 
     const recipe = recipes.find(r => r.recipe_name.toLowerCase() === recipeName.toLowerCase());
     console.log(recipe);
